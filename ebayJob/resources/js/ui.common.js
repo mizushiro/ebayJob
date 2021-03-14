68fb14cd91b4055af377ce6c1562a1b2ed5be724
asever77 <johyunmin77@gmail.com>
2021-03-14 20:03:40 +0900

    완료

--- ebayJob/resources/js/ui.common.js
+++ ebayJob/resources/js/ui.common.js
@@ -12,14 +12,21 @@
 				callback:$plugins.common.header 
 			});
 
+			$plugins.uiAjax({ 
+				id: $('.base-footer'), 
+				url:'/ebayJob/html/inc/footer.html', 
+				page:true, 
+				callback:$plugins.common.footer 
+			});
+
+
 
 			console.log('------------------------------------------------------')
 
 			$plugins.uiCaption();
 			$plugins.uiInputClear();
 
-
-			
+			$(document).on("touchstart", function(){ });
 
 		},
 		
@@ -33,39 +40,92 @@
 			console.log('header load: ' , pageid);
 
 			switch (pageid) {
-				case 99 :
-					subTitle('가이드');
-					break;
-				case 0 :
-					subTitle('main');
-					break;
-				case 1 :
-					subTitle('사업분야');
-					break;
-				default:
-					subTitle('main');
-					break;
+				case 99 : subTitle('가이드');
+				break;
+				case 0 : mainTitle();
+
+				break;
+				case 11 : subTitle('사업분야');
+				break;
+				case 12 : subTitle('GLOBAL MARKETPLACE');
+				break;
+				case 13 : subTitle('스마일시리즈');
+				break;
+				case 14 : subTitle('연혁');
+				break;
+
+				case 21 : subTitle('직무인터뷰');
+				break;
+				case 22 : subTitle('복리후생');
+				break;
+				case 23 : subTitle('OUR DNA');
+				break;
+				case 24 : subTitle('이베이 라이프');
+				break;
+				case 25 : subTitle('이베이 소식');
+				break;
+
+				case 31 : subTitle('채용절차');
+				break;
+				case 32 : subTitle('진행중인 공고');
+				break;
+				case 33 : subTitle('문의하기');
+				break;
+				case 34 : subTitle('인재풀 등록');
+				break;
+
+				case 41 : subTitle('지원서 작성');
+				break;
+				case 42 : subTitle('지원서 수정');
+				break;
+				case 43 : subTitle('지원서 조회');
+				break;
+				case 44 : subTitle('마이페이지');
+				break;
+		
+				default: subTitle('main');
+				break;
 			}
 
 			function subTitle(v){
 				$title.append('<h2>'+ v +'</h2>');
 				$wrap.addClass('page-' + pageid);
 			}
-		},
-		navOpen: function(){
-		
-		},
-		settingAside: function(){
-			
-		},
-		pageInit: function(v){
-			
-		},
-		menuAjax: function(){
-		
 
-			
+			function mainTitle(){
+				var html = '<div class="hero-main">';
+				html += '<div class="n1"><div class="hero-main-item"><strong>Global Marketplace<em>세상 모든 가치를 연결하는 이베이코리아</em></strong></div></div>';
+				html += '<div class="n2"><div class="hero-main-item"><strong>Innovative<br>Commerce <em>혁신적인 기술 기반의 차별화 된 경험</em></strong></div></div>';
+				html += '<div class="n3"><div class="hero-main-item"><strong>Opportunity<br>for all <em>모두를 위한 기회 창출 </em></strong></div></div>';
+				html += '</div>'
+				
+				$title.append(html);
+				$wrap.addClass('page-' + pageid);
+
+				$('.hero-main').slick({
+					dots: true,
+
+					// autoplay: true,
+					// autoplaySpeed: 2000,
+				});
+			}
+
+			$wrap.find('.btn-nav').off('click.nav').on('click.nav', function(){
+				var v = !!$('body.nav-open').length;
+				$plugins.common.navOpen(v);
+			});
+			// $wrap.find('.btn-close').off('click.nav').on('click.nav', $plugins.common.navClose);
 		},
+		navOpen: function(v){
+			var $body = $('body');
+
+			v ? 
+			$body.removeClass('nav-open') :
+			$body.addClass('nav-open');
+
+			$('.ebay-nav').scrollTop(0);
+		},
+
 		footer: function(){
 			console.log('footer load');
 		}
